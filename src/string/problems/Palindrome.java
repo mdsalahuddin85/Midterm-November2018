e851d9602c92b930a365dd13b015e73416390272
mdsalahuddin85 <mdsrobin1985@gmail.com>
2018-12-23 18:52:21 -0500

    5th commit

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -7,5 +7,37 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String inputString = "Brother";
+
+
+        int length = inputString.length();
+        int i, begin, end, middle;
+
+        begin = 0;
+        end = length - 1;
+        middle = (begin + end)/2;
+
+        for (i = begin; i <= middle; i++) {
+            if (inputString.charAt(begin) == inputString.charAt(end)) {
+                begin++;
+                end--;
+            }
+            else {
+                break;
+            }
+        }
+        if (i == middle + 1) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not a palindrome");
+        }
     }
 }
+
+
+
+
+
+
+
