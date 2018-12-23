b6060832bd4992e45c66052dba91b301a4cce968
mdsalahuddin85 <mdsrobin1985@gmail.com>
2018-12-23 18:20:20 -0500

    4th commit

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -14,21 +14,33 @@ public class LowestNumber {
 		 */
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
-		//find lowest number from the array
 
-		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-		List<String> lowestValue = new ArrayList<String>();
-		try {
-			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		int sum = array[0];
+		for (int i = 1; i <array.length; i++) {
+			if (array[i] < sum) {
+				sum = array[i];
+			}
+			}
+			System.out.println(sum);
 		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for(String st:lowestValue){
-			System.out.println(st);
 		}
-	}
 
-}
+//
+//		//find lowest number from the array
+//
+//		/*ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+//		List<String> lowestValue = new ArrayList<String>();
+//		try {
+//			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
+//			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
+//		for(String st:lowestValue){
+//			System.out.println(st);*/
+//		}
+//	}
+//
+//}
